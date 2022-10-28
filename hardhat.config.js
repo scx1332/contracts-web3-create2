3221ea6c8117d393d21544ee510217916b6e062d
scx1332 <sieciech@gmail.com>
2022-10-28 19:06:51 +0200

    Working on create2 deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config({path:__dirname+'/.env'})
 
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-web3");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,8 +5,39 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+const {ethers} = require("hardhat");
+const fs = require('fs').promises;
+
+
+async function getContractFactoryAbi() {
+    const contractName = "ContractFactory"
+    let compiledContract = await fs.readFile(`artifacts/contracts/${contractName}.sol/${contractName}.json`);
+    const contract = JSON.parse(compiledContract);
+    return contract.abi;
+}
+
+function buildCreate2Address(creatorAddress, saltHex, byteCode) {
+    let byteCodeSha = ethers.utils.keccak256(byteCode);
+    let data = ['ff', creatorAddress, saltHex, byteCodeSha].map(x => x.replace('0x', '')).join('');
+    let contractAddr = ethers.utils.keccak256(`0x${data}`).slice(-40);
+    return `0x${contractAddr}`;
+}
+
+function encodeParam(dataType, data) {
+    const provider = new ethers.providers.JsonRpcProvider();
+
+    return web3.eth.abi.encodeParameter(dataType, data)
+}
+function numberToUint256(value) {
+    const hex = value.toString(16)
+    return `0x${'0'.repeat(64-hex.length)}${hex}`
+}
 
 async function main() {
+    if (process.env.CONTRACT_FACTORY == undefined) {
+        console.log("Deploy contract factory first and set CONTRACT_FACTORY address in .env file");
+        return;
+    }
     const signers = await hre.ethers.getSigners();
     const signer = signers[0];
     const provider = signer.provider;
@@ -15,17 +46,44 @@ async function main() {
     let balance = await provider.getBalance(pubAddr);
     console.log(`Using account ${pubAddr} Account balance: ${balance}`);
 
-    if (balance.eq(0)) {
-        console.log("Account balance is 0. Exiting.");
-        return;
-    }
+    //load MultiTransferERC20.json
+    const contractName = "MultiTransferERC20"
+    let compiledContract = await fs.readFile(`artifacts/contracts/${contractName}.sol/${contractName}.json`);
+    const contract = JSON.parse(compiledContract);
+    const abi = contract.abi;
+    const contractBytecode = contract.bytecode;
 
-    const cf = await hre.ethers.getContractFactory("ContractFactory");
-    console.log("Balance:", balance.toString());
+    //console.log("ABI:", abi);
+    //console.log("Bytecode:", bytecode);
+    const bytecode = `${contractBytecode}${encodeParam('address', pubAddr).slice(2)}`
 
-    const contractFactory = await cf.deploy();
-    await contractFactory.deployed();
-    console.log("ContractFactory deployed to:", contractFactory.address);
+
+    const factoryAddress = process.env.CONTRACT_FACTORY;
+    let salt = 9;
+    let addr = buildCreate2Address(factoryAddress, numberToUint256(salt), bytecode);
+    console.log("Computed address:", addr);
+
+
+    let deployer = new ethers.Contract(factoryAddress, await getContractFactoryAbi(), signer);
+
+    //const bytecodeParam = ethers.utils.defaultAbiCoder.encode(['bytes', 'address'], [bytecode, pubAddr]);
+    //let computedAddr = deployer.callStatic.deploy(bytecode, salt);
+
+    let tx = await deployer.deploy(bytecode, numberToUint256(salt));
+  //  let txDeploy = deployer.populateTransaction.deploy(bytecode, salt);
+
+//    let tx = await signer.sendTransaction(txDeploy);
+
+
+    //let balance = await provider.getBalance(pubAddr);
+    //console.log(`Using account ${pubAddr} Account balance: ${balance}`);
+
+
+
+
+
+
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
