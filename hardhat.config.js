e7cf08e9cc8f41bc448948abe27645f43e00600e
scx1332 <sieciech@gmail.com>
2022-10-28 13:01:56 +0200

    f

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,30 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    defaultNetwork: "rinkeby",
+    networks: {
+        hardhat: {},
+        rinkeby: {
+            url: "https://eth-rinkeby.alchemyapi.io/v2/123abc123abc123abc123abc123abcde",
+            accounts: [privateKey1, privateKey2, ...]
+        }
+    },
+    solidity: {
+        version: "0.8.17",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 1000
+            }
+        }
+    },
+    paths: {
+        sources: "./contracts",
+        tests: "./test",
+        cache: "./cache",
+        artifacts: "./artifacts"
+    },
+    mocha: {
+        timeout: 40000
+    }
 };
