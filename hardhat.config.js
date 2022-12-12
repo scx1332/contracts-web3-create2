0f9d76ceed97e361331e3ccf33d8bbe6a3a207cf
scx1332 <sieciech.czajka@golem.network>
2022-12-12 19:30:02 +0100

    Fix chain id

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,9 +10,7 @@ module.exports = {
             url: "http://127.0.0.1:8545",
             accounts: ["0xee565091929f51d02c504f4c37ecc79abd5caa7a67c8917d862d4393c8992519"],
             glmToken: "0x0B220b82F3eA3B7F6d9A1D8ab58930C064A2b5Bf",
-            contractPrefix: "0x111",
-            contractFactory: "0x1469e7B8B7B16110c76ED029DcB2DD0B10D47c6A",
-            chainId: 77
+            chainId: 987789
         }
     },
     solidity: {
